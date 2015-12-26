88f115079eecc6ea1d07bb7b05253a47c8abe98e
Saph32 <saph32@users.noreply.github.com>
2015-12-26 08:34:58 -0500

    Removing base classes

--- src/SPCVisualizer/DFillRect.h
+++ src/SPCVisualizer/DFillRect.h
@@ -1,16 +1,23 @@
 #pragma once
 
-#include "FillRectBase.h"
+#include "IFillRect.h"
 #include "DIntf.h"
 
-class DFillRect : public G::FillRectBase
+class DFillRect : public G::IFillRect
 {
 public:
-    DFillRect(DCore& rCore, const G::Color& rColor) : FillRectBase(rColor), m_rCore(rCore) {}
+    DFillRect(DCore& rCore, const G::Color& rColor) : m_color(rColor), m_rCore(rCore) {}
     virtual ~DFillRect() override {}
 
+    // Interface implementation: IFillRect
+    virtual G::Color GetColor() override { return m_color; }
+    virtual void SetColor(const G::Color& rColor) override { m_color = rColor; }
+
+    // Interface implementation: IElem
     virtual void Draw(const G::RectF& rRect) override;
 
 private:
     DCore& m_rCore;
+
+    G::Color m_color;
 };
--- src/SPCVisualizer/DRender.h
+++ src/SPCVisualizer/DRender.h
@@ -1,17 +1,28 @@
 #pragma once
 
-#include "RenderBase.h"
+#include "IRender.h"
 #include "DIntf.h"
 
-class DRender final : public G::RenderBase
+#include <mutex>
+
+class DRender final : public G::IRender
 {
 public:
     DRender(DCore& rCore);
     virtual ~DRender() override;
+
+    // Interface implementation
+    virtual void SetRenderFrameCallback(RenderFrameCbFn fnCallback) override {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_renderFrameCb = fnCallback;
+    };
 
     // Other methods
     void RenderFrame();
 
 private:
     DCore& m_rCore;
+
+    std::mutex m_mutex;
+    RenderFrameCbFn m_renderFrameCb;
 };
